aa5466b79cc988dac04338a0ffb6f20c3a12c9e9
Kevin H <kevin@autoworkz.org>
2022-08-06 13:07:58 -0400

    docs(explore): OS exploration.

--- twitter_unfollow/MirandaB/mboyd.py
+++ twitter_unfollow/MirandaB/mboyd.py
@@ -1,2 +1,33 @@
-print("hello world")
-#im sleepy
+import os
+import json
+import pathlib as plib
+from loguru import logger, logger as log
+
+
+def main():
+    # dir_items =
+    # resulting = os.path.join(f"file/location/sofar", "layers", "layerz", "lay")
+    # logger.warning(resulting)
+
+    # f"file/location/sofar/layers"
+    for image_file in os.listdir("."):
+        if image_file.endswith(".json"):
+            with open(image_file) as f:
+                data = json.load(f)
+                logger.info(data["name"])
+            log.warning(image_file)
+            log.success(image_file.replace(".json", ""))
+            image_base = os.path.join(".", image_file.replace(".json", ""), "layers")
+            for os_file in os.listdir(image_base):
+                log.debug(os_file)
+                if os.path.isfile(os.path.join(image_base, os_file)):
+                    log.success(os_file)
+
+            log.info(image_base)
+            log.error(image_base)
+    x = sum([100, 200, 300])
+    log.critical(x)
+
+
+if __name__ == "__main__":
+    main()
